c12c695d56037c1fa632c3b846cc519ab04175ff
dasaint001 <elijahbamidele48@yahoo.com>
2022-02-03 14:39:08 +0100

    edits

--- snapnet-NPC-app/app/Http/Controllers/Loactions/Lga.php
+++ snapnet-NPC-app/app/Http/Controllers/Loactions/Lga.php
@@ -17,6 +17,34 @@ class Lga extends Controller
 
     public function addNewLga(Request $request)
     {
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'invalid credential supplied'
+            ], 400);
+        }
        return LgaService::createLga($request);
+    }
+
+        public function updateLga(Request $request,$id)
+    {
+        $validator = \Validator::make($request->all(), $this->rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'invalid credential supplied'
+            ], 400);
+        }
+       return LgaService::editLga($request,$id);
+    }
+
+    public function allLgas(Request $request)
+    {
+       return LgaService::getLga($request);
+    }
+
+    public function deleteLga(Request $request, $id)
+    {
+       return LgaService::removeLga($request, $id);
     }
 }
